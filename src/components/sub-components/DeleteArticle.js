cb5eba0b3d1a532b78c3ba5c37d3f7e0ac03b59d
jboylin <joeboylin@gmail.com>
2021-05-28 12:58:06 +0100

    can no longer delete an article or comment twice, fixing any ui errors

--- src/components/sub-components/DeleteArticle.js
+++ src/components/sub-components/DeleteArticle.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { Redirect } from "react-router";
 import { deleteById } from "../../util/api";
 
 const DeleteArticleById = ({ article }) => {
+  const [deleting, setDeleting] = useState(false);
+  const [deleted, setDeleted] = useState(false);
   const article_id = article.article_id;
   const removeArticle = () => {
+    setDeleting(true);
     const type = "articles";
-    deleteById(type, article_id);
-    <Redirect to="/" />;
+    deleteById(type, article_id).then(() => {
+      setDeleting(false);
+      setDeleted(true);
+    });
   };
+
+  if (deleting) return <h1>deleting...</h1>;
+
+  if (deleted) return <Redirect to="/home" />;
 
   return <button onClick={() => removeArticle()}>Delete</button>;
 };
--- src/components/sub-components/DeleteComment.js
+++ src/components/sub-components/DeleteComment.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { deleteById } from "../../util/api";
 
 const DeleteComment = ({ comment, setCommentCounter, setComments }) => {
   const comment_id = comment.comment_id;
+  const [deleting, setDeleting] = useState(false);
   const removeComment = () => {
+    setDeleting(true);
     const type = "comments";
     deleteById(type, comment_id).then(() => {
+      setDeleting(false);
       setComments((currComments) => {
         const currCommentsCopy = [...currComments];
         return currCommentsCopy.filter((currComment) => {
@@ -16,7 +20,7 @@ const DeleteComment = ({ comment, setCommentCounter, setComments }) => {
       });
     });
   };
-
+  if (deleting) return <h1>deleting...</h1>;
   return <button onClick={() => removeComment()}>Delete</button>;
 };
 
